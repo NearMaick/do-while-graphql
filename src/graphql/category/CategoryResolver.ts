2f3a9ac7198e5c0c805e3fa687f98f13982043a1
Maick Souza <maick_a_s@msn.com>
2020-12-27 12:21:48 -0300

    chore(server): type checking

--- src/graphql/category/CategoryResolver.ts
+++ src/graphql/category/CategoryResolver.ts
@@ -1,6 +1,13 @@
 import { Arg, Field, InputType, Mutation, Query, Resolver } from 'type-graphql'
 import Category from './Category'
 import CategorySchema from '../../model/CategorySchema'
+import { Document } from 'mongoose'
+
+interface CategoryType {
+  _id: string
+  name: string
+  description: string
+}
 
 @InputType()
 class CategoryInput {
@@ -14,13 +21,15 @@ class CategoryInput {
 @Resolver(Category)
 class CategoryResolver {
   @Query(() => [Category])
-  async categories() {
+  async categories(): Promise<Document<CategoryType>[]> {
     const categories = await CategorySchema.find()
     return categories
   }
 
   @Mutation(() => Category)
-  async createCategory(@Arg('categoryInput') categoryInput: CategoryInput) {
+  async createCategory(
+    @Arg('categoryInput') categoryInput: CategoryInput
+  ): Promise<Document<CategoryType>> {
     const category = await CategorySchema.create(categoryInput)
     return category
   }
--- src/graphql/video/VideoResolver.ts
+++ src/graphql/video/VideoResolver.ts
@@ -1,6 +1,14 @@
 import { Resolver, Query, Field, InputType, Mutation, Arg } from 'type-graphql'
 import Video from './Video'
 import VideoSchema from '../../model/VideoSchema'
+import { Document } from 'mongoose'
+
+interface VideoType {
+  _id: string
+  name: string
+  description: string
+  category: string
+}
 
 @InputType()
 class VideoInput {
@@ -17,13 +25,15 @@ class VideoInput {
 @Resolver(Video)
 class VideoResolver {
   @Query(() => [Video])
-  async videos() {
+  async videos(): Promise<Document<VideoType>[]> {
     const videos = await VideoSchema.find()
     return videos
   }
 
   @Mutation(() => Video)
-  async createVideos(@Arg('videoInput') videoInput: VideoInput) {
+  async createVideos(
+    @Arg('videoInput') videoInput: VideoInput
+  ): Promise<Document<VideoType>> {
     const video = await VideoSchema.create(videoInput)
     return video
   }
